548d2299ce3268ca7b26f345afded743601a797a
ihoorados <hooradramezani@gmail.com>
2022-12-24 07:52:48 +0000

    Initial Commit

---
+++ SearchInRedux/SearchInReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchInReduxApp.swift
+//  SearchInRedux
+//
+//  Created by Hoorad Ramezani on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchInReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
